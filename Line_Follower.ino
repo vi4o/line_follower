022ecd3c1c4e5b9f9274a4c12416ca8642a56162
Viktor Robev <viktorrobev@gmail.com>
2015-11-14 01:42:26 +0200

    Removed unused code

--- Line_Follower.ino
+++ Line_Follower.ino
@@ -18,43 +18,69 @@
 #define SEN_8 13
 
 #define BUTTON 2
-#define BTN_THRES 767 // 75% of 1023
+#define BTN_THRES 800// >75% of 1023
 
-#define Kp 0.04 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
-#define Kd 0.3 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
-#define Ki 0
-#define rightMaxSpeed 120 // max speed of the robot
-#define leftMaxSpeed 120 // max speed of the robot
-#define rightBaseSpeed 70 // this is the speed at which the motors should spin when the robot is perfectly on the line
-#define leftBaseSpeed 70  // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define Kp 0.065 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
+#define Kd 0.65 // experiment to determine this, slowly increase the speeds and adjust this value. (Note: Kp < Kd)
+#define Ki 0.015
+#define rightMaxSpeed 180 // max speed of the robot
+#define leftMaxSpeed 180 // max speed of the robot
+
+#define rightBaseSpeed 120 // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define leftBaseSpeed 120  // this is the speed at which the motors should spin when the robot is perfectly on the line
 #define NUM_SENSORS  8     // number of sensors used
 #define TIMEOUT       2500  // waits for 2500 us for sensor outputs to go low
 #define EMITTER_PIN   255     // emitter is controlled by digital pin 2
 
-void driveLeftMotor(int intensity) {
-  int direction = (intensity >= 0) ? 1 : -1;
-  if (direction >= 0) {
-    digitalWrite(leftMotor1, LOW);
-    digitalWrite(leftMotor2, HIGH);
-    analogWrite(leftMotorPWM, intensity);
+#define MOTOR_STEPS 4
+#define STEP_THRESHOLD 100
+inline void drive(int leftMotorPin, int rightMotorPin,
+                  int leftIntensity, int rightIntensity,
+                  int *lastLeftIntensity, int *lastRightIntensity) {
+  int leftMotorStep;
+  int rightMotorStep;
+  // If intensity difference is greater than STEP_THRESHOLD, do MOTOR_STEPS intensity transitions
+  if (abs(leftIntensity - *lastLeftIntensity) > STEP_THRESHOLD) {
+    leftMotorStep = (leftIntensity - *lastLeftIntensity) / MOTOR_STEPS;
   } else {
-    digitalWrite(leftMotor1, HIGH);
-    digitalWrite(leftMotor2, LOW);
-    analogWrite(leftMotorPWM, -intensity);
+    *lastLeftIntensity = leftIntensity;
+    leftMotorStep = 0;
+  }
+  if (abs(rightIntensity - *lastRightIntensity) > STEP_THRESHOLD) {
+     rightMotorStep = (rightIntensity - *lastRightIntensity) / MOTOR_STEPS;
+  } else {
+    *lastRightIntensity = rightIntensity;
+    rightMotorStep = 0;
+  }
+  for (int cnt = 0; cnt < MOTOR_STEPS; cnt++) {
+    *lastLeftIntensity += leftMotorStep;
+    *lastRightIntensity += rightMotorStep;
+    analogWrite(leftMotorPin, *lastLeftIntensity);
+    analogWrite(rightMotorPin, *lastRightIntensity);
+    delay(2);
   }
 }
 
-void driveRightMotor(int intensity) {
-  int direction = (intensity >= 0) ? 1 : -1;
-  if (direction >= 0) {
+void driveMotors(int leftIntensity, int rightIntensity, int *lastLeftIntensity, int *lastRightIntensity) {
+  int leftDirection = (leftIntensity >= 0) ? 1 : -1;
+  int rightDirection = (rightIntensity >= 0) ? 1 : -1;
+  if (leftDirection >= 0) {
+    digitalWrite(leftMotor1, LOW);
+    digitalWrite(leftMotor2, HIGH);
+  } else {
+    digitalWrite(leftMotor1, HIGH);
+    digitalWrite(leftMotor2, LOW);
+    leftIntensity = -leftIntensity;
+  }
+  if (rightDirection >= 0) {
     digitalWrite(rightMotor1, LOW);
     digitalWrite(rightMotor2, HIGH);
-    analogWrite(rightMotorPWM, intensity);
   } else {
     digitalWrite(rightMotor1, HIGH);
     digitalWrite(rightMotor2, LOW);
-    analogWrite(rightMotorPWM, -intensity);
+    rightIntensity = -rightIntensity;
   }
+  drive(leftMotorPWM, rightMotorPWM, leftIntensity, rightIntensity, lastLeftIntensity, lastRightIntensity);
 }
 
 QTRSensorsRC qtrrc((unsigned char[]) {
@@ -67,21 +93,12 @@ void setup() {
   pinMode(rightMotorPWM, OUTPUT);
   pinMode(leftMotor1, OUTPUT);
   pinMode(leftMotor2, OUTPUT);
-  
-  // driveLeftMotor(180);
-  //driveRightMotor(100);
 
   int i;
-  for (int i = 0; i < 100; i++) { // calibrate for sometime by sliding the sensors across the line, or you may use auto-calibration instead
-    /* comment this part out for automatic calibration
-    if ( i  < 25 || i >= 75 ) // turn to the left and right to expose the sensors to the brightest and darkest readings that may be encountered
-       turn_right();
-     else
-       turn_left(); */
+  for (int i = 0; i < 100; i++) {
     qtrrc.calibrate();
     delay(20);
   }
-  //wait();
   while (analogRead(BUTTON) < BTN_THRES) {
     delay(500);
   }
@@ -90,13 +107,14 @@ void setup() {
 
 int lastError = 0;
 int integral = 0;
+int lastRightMotorSpeed = 0;
+int lastLeftMotorSpeed = 0;
 
 unsigned int sensors[8];
 void loop() {
   int position = qtrrc.readLine(sensors); // get calibrated readings along with the line position, refer to the QTR Sensors Arduino Library for more details on line position.
   int error = position - 3500;
 
-  integral = integral + error;
   int motorSpeed = Kp * error + Kd * (error - lastError) + Ki * integral;
   lastError = error;
 
@@ -105,9 +123,10 @@ void loop() {
 
   bool rightDir = (rightMotorSpeed >= 0) ? 1 : -1;
   bool leftDir = (leftMotorSpeed >= 0) ? 1 : -1;
-  if (rightMotorSpeed * rightDir > rightMaxSpeed ) rightMotorSpeed = rightMaxSpeed * rightDir; // prevent the motor from going beyond max speed
-  if (leftMotorSpeed * leftDir > leftMaxSpeed ) leftMotorSpeed = leftMaxSpeed * leftDir; // prevent the motor from going beyond max speed
 
-  driveRightMotor(rightMotorSpeed);
-  driveLeftMotor(leftMotorSpeed);
+  // prevent the motor from going beyond max speed
+  if (rightMotorSpeed * rightDir > rightMaxSpeed ) rightMotorSpeed = rightMaxSpeed * rightDir;
+  if (leftMotorSpeed * leftDir > leftMaxSpeed ) leftMotorSpeed = leftMaxSpeed * leftDir;
+
+  driveMotors(leftMotorSpeed, rightMotorSpeed, &lastLeftMotorSpeed, &lastRightMotorSpeed);
 }
